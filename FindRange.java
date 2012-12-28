10225656ee0d2d61eab9765768fad6965501f425
S <s>
2012-12-28 11:32:34 -0500

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -12,13 +12,14 @@ public class FindRange extends ConsoleProgram {
 	private static final int SENTINEL =0;
 	
 	public void run() {
-int smallest = 0;
-int largest = 0;
+int smallest;
+int largest;
 int i = 0;
 
 while (true){
 int val = readInt("? ");
 if (val==SENTINEL){break;}
+else if (i==0){largest = val; smallest = val;}
 else if (val > largest) {largest = val;i++;}
 else if (val < smallest) {smallest = val; i++;}
 }
