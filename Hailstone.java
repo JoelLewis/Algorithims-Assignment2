2c5b3ba62f777855743426acb4f66926d8fa0979
S <s>
2012-12-28 11:17:54 -0500

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -13,18 +13,16 @@ public class Hailstone extends ConsoleProgram {
 	int x = readInt("Enter a number: ");
 	int i = 0;
 	while(x > 1){
-	if 
-		(x==1){println("The process took "+ i + " steps to reach 1");break;}
-		else if(x%2 == 0){
+	if (x%2 == 0){
 		println(x+" is even so I take half: " + x/2);
 		x=x/2;
 		i++;
-		if(x==1){println("The process took "+ i + " steps to reach 1"); break;}} 
+		if(x==1){println("The process took "+ i + " steps to reach 1");}} 
 		else{
 		println(x+" is odd so I make 3n + 1: "+(3*x+1));
 		x=3*x+1;
 		i++;
-		if(x==1)println("The process took "+ i + " steps to reach 1");break;}}
+		if(x==1)println("The process took "+ i + " steps to reach 1");}}
 	}}
 
 
