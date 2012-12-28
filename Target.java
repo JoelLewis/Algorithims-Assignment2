a11e16ca25df61aa228c9c1c8954807f5a59f21e
S <s>
2012-12-28 09:40:49 -0500

    {"type":"TI", "runs":"1"}

--- Target.java
+++ Target.java
@@ -17,8 +17,11 @@ public class Target extends GraphicsProgram {
 		double centerX = getWidth()/2.0;
 		
 		GOval inner = new GOval(centerX, centerY, pixelRadius(.3), pixelRadius(.3));
+		add(inner);
 		GOval middle = new GOval(centerX, centerY, pixelRadius(.65),pixelRadius(.65));
+		add (middle);
 		GOval outer = new GOval(centerX, centerY, pixelRadius(1.0),pixelRadius(1.0));
+		add (outer);
 	}
 	double pixelRadius(double radius) {
 		double diam=radius*2/72;
