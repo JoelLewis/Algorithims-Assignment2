1e00585ed4905a772ae51cf1e26272146e7ed821
S <s>
2012-12-28 11:13:09 -0500

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -11,9 +11,10 @@ import acm.program.*;
 public class Hailstone extends ConsoleProgram {
 	public void run() {
 	int x = readInt("Enter a number: ");
-	for(int i = 1 ; x > 1; i++){
+	int i = 0;
+	while(x > 1){
 	if 
-		(x==1){println("The process took 0 steps to reach 1");}
+		(x==1){println("The process took "+ i + " steps to reach 1");}
 		else if(x%2 == 0){
 		println(x+" is even so I take half: " + x/2);
 		x=x/2;
