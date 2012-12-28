a65818ab82232b049a3ec0fe7f2f7530f8242c6f
S <s>
2012-12-28 12:40:24 -0500

    {"type":"TI", "runs":"1"}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -16,7 +16,7 @@ public class ProgramHierarchy extends GraphicsProgram {
 		double centerX = getWidth()/2.0;
 		
 		GLabel consoleProg = new GLabel("ConsoleProgram", centerX+100, centerY+30); 
-		GLabel prog = new GLabel("Program", consoleProg.getX()+consoleProg.getWidth()/2, centerY-50);
+		GLabel prog = new GLabel("Program", consoleProg.getX()+consoleProg.getWidth()/4, centerY-50);
 		GLabel graphProg = new GLabel("GraphicsProgram", centerX-50, centerY+30);
 		GLabel dialogProg = new GLabel("DialogProgram", centerX+250, centerY+30);
 		
