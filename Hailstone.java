5f0cb92abbf876f37229e0e2e142a44bf88b257f
S <s>
2012-12-28 10:42:11 -0500

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -11,7 +11,7 @@ import acm.program.*;
 public class Hailstone extends ConsoleProgram {
 	public void run() {
 	int x = readInt("Enter a number: ");
-	for(int i = 0;x < 1;i++){
+	for(int i = 0;x > 1;i++){
 	if(x%2 == 0){
 		println(x+" is even so I take half: " + x/2);
 		x=x/2;
