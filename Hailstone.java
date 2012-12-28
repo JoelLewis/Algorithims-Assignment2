62757af9de2b9c90fa938ffd551430201d94176c
S <s>
2012-12-28 11:13:58 -0500

    {"type":"TI", "runs":"1"}

--- Hailstone.java
+++ Hailstone.java
@@ -18,10 +18,12 @@ public class Hailstone extends ConsoleProgram {
 		else if(x%2 == 0){
 		println(x+" is even so I take half: " + x/2);
 		x=x/2;
+		i++;
 		if(x==1){println("The process took "+ i + " steps to reach 1");}} 
 		else{
 		println(x+" is odd so I make 3n + 1: "+(3*x+1));
 		x=3*x+1;
+		i++;
 		if(x==1)println("The process took "+ i + " steps to reach 1");
 	}}}}
 
